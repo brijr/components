01c62892601222e42fd3fae981b8830b7ee82306
Bridger Tower <bridgertower@gmail.com>
2024-01-25 22:56:08 -0700

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 // Layout Imports
 import { Main } from "@/components/craft/layout";
 import { Wrapper } from "@/components/wrapper";
@@ -7,41 +5,7 @@ import { Info } from "@/components/info";
 import { End } from "@/components/end";
 import * as Craft from "@/components/craft/layout";
 
-// Component Imports
-import HeroOne from "@/components/heros/hero-one";
-import HeroTwo from "@/components/heros/hero-two";
-import HeroThree from "@/components/heros/hero-three";
-import HeroFour from "@/components/heros/hero-four";
-
-// Component Code Imports
-import HeroOneCode from "!!raw-loader!@/components/heros/hero-one";
-import HeroTwoCode from "!!raw-loader!@/components/heros/hero-two";
-import HeroThreeCode from "!!raw-loader!@/components/heros/hero-three";
-import HeroFourCode from "!!raw-loader!@/components/heros/hero-four";
-
-// Component Data
-const components = [
-  {
-    component: <HeroOne />,
-    path: "heros/hero-one",
-    code: HeroOneCode,
-  },
-  {
-    component: <HeroTwo />,
-    path: "heros/hero-two",
-    code: HeroTwoCode,
-  },
-  {
-    component: <HeroThree />,
-    path: "heros/hero-three",
-    code: HeroThreeCode,
-  },
-  {
-    component: <HeroFour />,
-    path: "heros/hero-four",
-    code: HeroFourCode,
-  },
-];
+import { components } from "@/components.config";
 
 export default function Home() {
   return (
@@ -55,7 +19,7 @@ export default function Home() {
             key={component.path}
             path={component.path}
           >
-            {component.component}
+            <component.component />
           </Wrapper>
         ))}
       </Craft.Section>
