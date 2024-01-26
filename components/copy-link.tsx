238f44110e0f418235fac41fa385abad6798fb27
Bridger Tower <bridgertower@gmail.com>
2024-01-26 08:12:15 -0700

    updates

--- components/copy-link.tsx
+++ components/copy-link.tsx
@@ -2,6 +2,14 @@
 
 import { useClipboard } from "use-clipboard-copy";
 import { toast } from "sonner";
+import { Link } from "lucide-react";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export const CopyLink = ({ path }: { path: string }) => {
   const clipboard = useClipboard({ copiedTimeout: 1500 });
@@ -13,9 +21,20 @@ export const CopyLink = ({ path }: { path: string }) => {
   };
 
   return (
-    <button onClick={handleCopyLink} className="text-xs">
-      <span className="opacity-70">components/</span>
-      {path}
-    </button>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <button onClick={handleCopyLink} className="text-xs">
+            <span className="opacity-70">components/</span>
+            {path}
+          </button>
+        </TooltipTrigger>
+        <TooltipContent side="bottom" sideOffset={8}>
+          <p className="not-prose flex gap-2 text-xs items-center">
+            <Link className="w-3" /> Copy Link to Component
+          </p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 };
