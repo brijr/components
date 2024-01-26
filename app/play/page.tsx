23c8b167fcea300e19c378285a407b5b28a07b42
Bridger Tower <bridgertower@gmail.com>
2024-01-26 09:48:56 -0700

    updates

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -3,14 +3,14 @@ import { Main } from "@/components/craft/layout";
 import { Wrapper } from "@/components/wrapper";
 
 // Component Imports
-import HeroFour from "@/components/heros/hero-four";
+import Header from "@/components/headers/header-one";
 
 export default function Home() {
   return (
-    <Main className="flex flex-col py-12 gap-12 min-h-screen items-center">
+    <Main className="flex min-h-screen flex-col items-center gap-12 py-12">
       <Wrapper code="playground" path="playground">
         {/* Component in Dev */}
-        <HeroFour />
+        <Header />
         {/* Component in Dev */}
       </Wrapper>
     </Main>
---
+++ components/headers/header-two.tsx
@@ -0,0 +1,25 @@
+import * as Craft from "@/components/craft/layout";
+import Balancer from "react-wrap-balancer";
+import { Button } from "../ui/button";
+
+const Header = () => {
+  return (
+    <Craft.Section>
+      <Craft.Container className="text-center">
+        <h2 className="!mt-0">This is a Header</h2>
+        <p className="text-gray-500">
+          <Balancer>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            voluptate quidem natus.
+          </Balancer>
+        </p>
+        <div className="flex gap-2 items-center justify-center">
+          <Button>Get Started</Button>
+          <Button variant={"outline"}>Learn More</Button>
+        </div>
+      </Craft.Container>
+    </Craft.Section>
+  );
+};
+
+export default Header;
