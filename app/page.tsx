df6f2c4e9290b1ef4477c17088f50b72d6c24bb9
Bridger Tower <bridgertower@gmail.com>
2024-02-15 23:21:11 -0700

    fix: sorting error

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,14 @@ import { Main } from "@/components/craft/layout";
 import { End } from "@/components/end";
 import ComponentList from "@/components/component-list";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   return (
     <Main>
-      <ComponentList searchParams={{}} />
+      <ComponentList searchParams={searchParams} />
       <End />
     </Main>
   );
