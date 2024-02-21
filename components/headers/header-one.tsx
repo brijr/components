68352df6bcf0103d41863ef52fd7462b1b54eae4
Bridger Tower <bridgertower@gmail.com>
2024-02-21 09:48:54 -0700

    update header one

--- components/headers/header-one.tsx
+++ components/headers/header-one.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <Craft.Section>
       <Craft.Container>
-        <h1 className="!mt-0">This is a Header</h1>
+        <h1 className="!my-0">This is a Header</h1>
         <p className="text-gray-500">
           <Balancer>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
