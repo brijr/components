43e33bfc20aab3a68d277b5c9bb6d9fc85de24e0
Bridger Tower <bridgertower@gmail.com>
2024-04-16 13:02:51 -0600

    update feature six

--- components/features/feature-six.tsx
+++ components/features/feature-six.tsx
@@ -46,7 +46,7 @@ const singleFeatureText: FeatureText[] = [
 
 const Feature = () => {
   return (
-    <Craft.Section className="border-b">
+    <Craft.Section>
       <Craft.Container className="not-prose">
         <div className="flex flex-col gap-6">
           <h3 className="text-4xl">
