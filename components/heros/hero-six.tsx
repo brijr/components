c154bed5dba176383f1dbf969c56b1c591d027d7
Bridger Tower <bridgertower@gmail.com>
2024-06-05 09:24:32 -0600

    hero 6 update

--- components/heros/hero-six.tsx
+++ components/heros/hero-six.tsx
@@ -18,7 +18,9 @@ const Hero = () => {
             <ArrowRight className="w-4 group-hover:-rotate-45 transition-all" />
           </Link>
         </Badge>
-        <h1>What if building landing pages was as easy as copy and paste?</h1>
+        <h1 className="!mb-0">
+          What if building landing pages was as easy as copy and paste?
+        </h1>
         <h3 className="text-muted-foreground p-4 bg-muted/50 border rounded-md">
           Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
