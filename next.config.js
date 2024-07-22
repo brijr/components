0033464fcb52516eeb05a4fa1315712698b10e6b
Bridger Tower <bridgertower@gmail.com>
2024-07-22 09:03:40 -0600

    add redirect from /all to /

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@ const withMDX = require("@next/mdx")();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/all",
+        destination: "/",
+        permanent: true,
+      },
+    ];
+  },
+
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 
   images: {
