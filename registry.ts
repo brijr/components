0bfc59861db020ce793b777c874b6682d830bf20
Bridger Tower <bridgertower@gmail.com>
2025-07-29 11:10:40 -0600

    create homepage

--- registry.ts
+++ registry.ts
@@ -1,8 +1,24 @@
-import Button from './components/Button';
-import Card from './components/Card';
+import { Button } from "./components/ui/button";
+import { Card } from "./components/ui/card";
 
-export const registry = [
-  { name: 'Button', slug: 'button', Component: Button },
-  { name: 'Card', slug: 'card', Component: Card },
+type RegistryItem = {
+  name: string;
+  slug: string;
+  Component: React.ComponentType<any>;
+  description?: string;
+};
+
+export const registry: RegistryItem[] = [
+  {
+    name: "Button",
+    slug: "button",
+    Component: Button,
+    description: "A customizable button component.",
+  },
+  {
+    name: "Card",
+    slug: "card",
+    Component: Card,
+    description: "A customizable card component.",
+  },
 ];
-
