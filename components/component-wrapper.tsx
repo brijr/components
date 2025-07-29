edbd73687edd694fc0f0cf7c18748241327fdc9d
Bridger Tower <bridgertower@gmail.com>
2025-07-29 15:04:08 -0600

    refactor: update layout in ComponentWrapper to use grid for better alignment and center text for improved readability

--- components/component-wrapper.tsx
+++ components/component-wrapper.tsx
@@ -14,14 +14,14 @@ export function ComponentWrapper({
 }: ComponentWrapperProps) {
   return (
     <section className="max-w-screen-xl w-full border mx-auto rounded-lg overflow-hidden bg-background">
-      <div className="h-12 bg-accent/50 border-b flex items-center justify-between px-4 text-muted-foreground">
+      <div className="h-12 bg-accent/50 border-b grid grid-cols-3 items-center px-4 text-muted-foreground">
         <div className="flex items-center gap-1.5">
           <Circle size={12} />
           <Circle size={12} />
           <Circle size={12} />
         </div>
-        <div className="text-sm">{name}</div>
-        <div className="flex items-center gap-2">
+        <div className="text-sm text-center">{name}</div>
+        <div className="flex items-center gap-2 justify-end">
           <CopyButton filePath={filePath} />
         </div>
       </div>
