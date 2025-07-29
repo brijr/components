401262108add119f4d4a3ea483c208c28bcfe235
Bridger Tower <bridgertower@gmail.com>
2025-07-29 11:06:48 -0600

    chore: create registry

---
+++ registry.ts
@@ -0,0 +1,8 @@
+import Button from './components/Button';
+import Card from './components/Card';
+
+export const registry = [
+  { name: 'Button', slug: 'button', Component: Button },
+  { name: 'Card', slug: 'card', Component: Card },
+];
+
