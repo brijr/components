ab1df997d1d221cd074651704e4e1cab7c43611e
Bridger Tower <bridgertower@gmail.com>
2025-07-29 13:30:20 -0600

    feat: enhance homepage layout by introducing a Wrapper component for better organization and styling of registry items

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Circle, Copy } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import { Main } from "@/components/ds";
 
 import { registry } from "@/registry";
@@ -5,14 +7,41 @@ import { registry } from "@/registry";
 export default function HomePage() {
   return (
     <Main>
-      <div className="py-6 bg-accent/30">
+      <div className="py-6">
         <h1 className="text-center font-mono text-sm">components.bridger.to</h1>
       </div>
-      {registry.map(({ name, slug, Component, props }) => (
-        <div className="border-t" key={slug}>
-          <Component {...(props || {})} />
-        </div>
-      ))}
+      <div className="grid gap-4">
+        {registry.map(({ name, slug, Component, props }) => (
+          <Wrapper key={slug} name={name}>
+            <Component {...(props || {})} />
+          </Wrapper>
+        ))}
+      </div>
     </Main>
   );
 }
+
+const Wrapper = ({
+  children,
+  name,
+}: {
+  children: React.ReactNode;
+  name: string;
+}) => {
+  return (
+    <div className="max-w-screen-xl w-full border mx-auto rounded-lg overflow-hidden">
+      <div className="h-12 bg-accent/50 border-b flex items-center justify-between pl-4 pr-2 text-muted-foreground">
+        <div className="flex items-center gap-1.5">
+          <Circle size={12} />
+          <Circle size={12} />
+          <Circle size={12} />
+        </div>
+        <div className="text-sm">{name}</div>
+        <Button variant="ghost" size="icon">
+          <Copy size={12} />
+        </Button>
+      </div>
+      {children}
+    </div>
+  );
+};
