7711ff2ec6686313a262884e042968344d8c2b53
Bridger Tower <bridgertower@gmail.com>
2025-07-29 12:06:33 -0600

    refactor: update homepage title and adjust styling for improved presentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { Main } from "@/components/ds";
+
 import { registry } from "@/registry";
 
 export default function HomePage() {
   return (
     <Main>
-      <div className="py-12 bg-accent/40 border-t">
-        <h1 className="text-center font-mono">My Component Gallery</h1>
+      <div className="py-6 bg-accent/30 border-t">
+        <h1 className="text-center font-mono text-sm">components.bridger.to</h1>
       </div>
       {registry.map(({ name, slug, Component, props }) => (
         <div className="border-t" key={slug}>
